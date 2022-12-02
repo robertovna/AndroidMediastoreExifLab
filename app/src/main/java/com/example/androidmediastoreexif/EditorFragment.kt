6f9e2c0a7522d2b51b9e0a2ae7e4fb1c0b14caa9
robertovna <g_arshakyan@list.ru>
2022-12-03 00:33:10 +0300

    added saving tags

--- app/src/main/java/com/example/androidmediastoreexif/EditorFragment.kt
+++ app/src/main/java/com/example/androidmediastoreexif/EditorFragment.kt
@@ -1,19 +1,25 @@
 package com.example.androidmediastoreexif
 
 import android.content.Context
+import android.media.ExifInterface
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.androidmediastoreexif.databinding.FragmentEditorBinding
-import com.example.androidmediastoreexif.databinding.FragmentViewImageBinding
+import java.io.IOException
 
 class EditorFragment : Fragment() {
     private var _binding: FragmentEditorBinding? = null
     private val binding get() = _binding!!
+
+    private val navigationArgs: ViewImageFragmentArgs by navArgs()
+    private var _imgUri: Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,9 +32,56 @@ class EditorFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        _imgUri = Uri.parse(navigationArgs.photoUri)
+
         binding.saveAction.setOnClickListener {
-            val action = EditorFragmentDirections.actionEditorFragmentToViewImageFragment()
+            saveTags()
+            val action = EditorFragmentDirections.actionEditorFragmentToViewImageFragment(
+                _imgUri.toString()
+            )
             findNavController().navigate(action)
+        }
+        bindTags()
+    }
+
+    private fun saveTags() {
+        if (_imgUri != null) {
+            requireContext().contentResolver.openFileDescriptor(_imgUri!!, "rw")?.use { fd ->
+                val exif = ExifInterface(fd.fileDescriptor)
+                if (!binding.itemDate.text.isNullOrBlank() && binding.itemDate.text.toString() != "null") {
+                    exif.setAttribute(ExifInterface.TAG_DATETIME, binding.itemDate.text.toString())
+                }
+                if (!binding.itemLatitude.text.isNullOrBlank() && binding.itemLatitude.text.toString() != "null") {
+                    exif.setAttribute(ExifInterface.TAG_GPS_LATITUDE, binding.itemLatitude.text.toString())
+                }
+                if (!binding.itemLongitude.text.isNullOrBlank() && binding.itemLongitude.text.toString() != "null") {
+                    exif.setAttribute(ExifInterface.TAG_GPS_LONGITUDE, binding.itemLongitude.text.toString())
+                }
+                if (!binding.itemDevice.text.isNullOrBlank() && binding.itemDevice.text.toString() != "null") {
+                    exif.setAttribute(ExifInterface.TAG_MAKE, binding.itemDevice.text.toString())
+                }
+                if (!binding.itemModel.text.isNullOrBlank() && binding.itemModel.text.toString() != "null") {
+                    exif.setAttribute(ExifInterface.TAG_MODEL, binding.itemModel.text.toString())
+                }
+                exif.saveAttributes()
+            }
+        }
+    }
+
+    private fun bindTags() {
+        if (_imgUri != null) {
+            try {
+                requireContext().contentResolver.openInputStream(_imgUri!!)?.use {
+                    val exif = ExifInterface(it)
+                    binding.apply {
+                        itemDate.setText(exif.getAttribute(ExifInterface.TAG_DATETIME).toString())
+                        itemLatitude.setText(exif.getAttribute(ExifInterface.TAG_GPS_LATITUDE).toString())
+                        itemLongitude.setText(exif.getAttribute(ExifInterface.TAG_GPS_LONGITUDE).toString())
+                        itemDevice.setText(exif.getAttribute(ExifInterface.TAG_MAKE).toString())
+                        itemModel.setText(exif.getAttribute(ExifInterface.TAG_MODEL).toString())
+                    }
+                }
+            } catch (e: IOException) { }
         }
     }
 
--- app/src/main/java/com/example/androidmediastoreexif/ViewImageFragment.kt
+++ app/src/main/java/com/example/androidmediastoreexif/ViewImageFragment.kt
@@ -1,16 +1,18 @@
 package com.example.androidmediastoreexif
 
 import android.app.Activity
-import android.content.Context.INPUT_METHOD_SERVICE
 import android.content.Intent
+import android.media.ExifInterface
+import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.androidmediastoreexif.databinding.FragmentViewImageBinding
+import java.io.IOException
 
 class ViewImageFragment : Fragment() {
     companion object {
@@ -18,6 +20,9 @@ class ViewImageFragment : Fragment() {
     }
     private var _binding: FragmentViewImageBinding? = null
     private val binding get() = _binding!!
+    private var _imageUri: Uri? = null
+
+    private val navigationArgs: EditorFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,22 +35,54 @@ class ViewImageFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        if (!navigationArgs.photoUri.isNullOrBlank() &&  navigationArgs.photoUri != "null") {
+            val uri = Uri.parse(navigationArgs.photoUri)
+            _imageUri = uri
+            drawImage(uri)
+        }
         binding.chooseImage.setOnClickListener {
-            val imageGalleryIntent = Intent(Intent.ACTION_PICK)
-            imageGalleryIntent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            val imageGalleryIntent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+            imageGalleryIntent.type = "image/*"
             startActivityForResult(imageGalleryIntent, FILE_CHOOSED)
         }
         binding.editImage.setOnClickListener {
-            val action = ViewImageFragmentDirections.actionViewImageFragmentToEditorFragment()
+            val action = ViewImageFragmentDirections.actionViewImageFragmentToEditorFragment(
+                _imageUri.toString()
+            )
             findNavController().navigate(action)
         }
     }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == FILE_CHOOSED && resultCode == Activity.RESULT_OK) {
             data?.data?.also { uri ->
-                binding.imageFromGallery.setImageURI(uri)
+                _imageUri = uri
+                drawImage(uri)
             }
         }
+    }
+
+    private fun drawImage(photoUri: Uri) {
+        var creationDate = ""
+        var latitude = ""
+        var longitude = ""
+        var device = ""
+        var model = ""
+        binding.imageFromGallery.setImageURI(photoUri)
+        try {
+            requireContext().contentResolver.openInputStream(photoUri)?.use {
+                val exif = ExifInterface(it)
+                creationDate = exif.getAttribute(ExifInterface.TAG_DATETIME).toString()
+                latitude = exif.getAttribute(ExifInterface.TAG_GPS_LATITUDE).toString()
+                longitude = exif.getAttribute(ExifInterface.TAG_GPS_LONGITUDE).toString()
+                device = exif.getAttribute(ExifInterface.TAG_MAKE).toString()
+                model = exif.getAttribute(ExifInterface.TAG_MODEL).toString()
+            }
+        } catch (e: IOException) { }
+
+        var text = "Date: $creationDate\nLatitude: $latitude\nLongtitude: $longitude\n"
+        text += "Device: $device\nModel: $model\n"
+        binding.tags.text = text
     }
 
     override fun onDestroyView() {
